8b5877659e00b4319830d0f1e9b9dd1bfce094e5
edsteffen <edineidaniel@gmail.com>
2020-04-19 17:55:45 -0300

    Rotas para Usuários

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,8 @@ const express = require('express');
 const MenuController = require('./controllers/MenuController');
 const LoginController = require('./controllers/LoginController');
 const NiveilAcessoController = require('./controllers/NivelAcessoController');
+const UsuarioController = require('./controllers/UsuarioController');
+const AuthMiddleware = require('./middleware/AuthMiddleware')
 const routes = express.Router();
 
 
@@ -15,8 +17,13 @@ routes.delete('/menus/:id', MenuController.delete);
 routes.get('/niveis', NiveilAcessoController.index);
 routes.post('/niveis', NiveilAcessoController.create);
 routes.put('/niveis/:id', NiveilAcessoController.update);
-routes.delete('/niveis/:id', NiveilAcessoController.delete);
+routes.delete('/niveis/:id', NiveilAcessoController.delete); 
 
-routes.post('/login', LoginController.test);
+routes.get('/usuarios', AuthMiddleware.auth_index, UsuarioController.index);
+routes.post('/usuarios', AuthMiddleware.auth_create, UsuarioController.create);
+routes.put('/usuarios', AuthMiddleware.auth_update, UsuarioController.update);
+routes.delete('/usuarios/:id', AuthMiddleware.auth_delete, UsuarioController.delete);
+
+routes.post('/login', AuthMiddleware.auth_index, LoginController.test);
 
 module.exports = routes;

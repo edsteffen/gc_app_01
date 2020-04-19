850966b8891db040db8a8efcfb43869df70cbed9
edsteffen <edineidaniel@gmail.com>
2020-04-19 17:55:28 -0300

    Controller para Usuários

--- backend/src/controllers/UsuarioController.js
+++ backend/src/controllers/UsuarioController.js
@@ -1,52 +1,67 @@
-const crypto = require('crypto');
 const connection = require('../database/connection');
+const security = require('./SecurityController');
 
 
 module.exports = {
     async index(request, response) {
-        const { page = 1 } = request.query;
-
-        const [count] = await connection('usuarios').count();
-
-        const usuarios = await connection('usuarios')
-        .limit(5)
-        .offset((page - 1) * 5)    
-        .select('*');
-
-        response.header('X-Total-Count', count['count(*)']);
+        try{
+            const { page = 1 } = request.query;
+        
+            const [count] = await connection('usuarios').count();
     
-        return response.json(usuarios);
+            const usuarios = await connection('usuarios')
+            .limit(10)
+            .offset((page - 1) * 10)    
+            .select('*');
+    
+            response.header('X-Total-Count', count['count(*)']);
+
+            return response.json(usuarios);
+        } catch (e) {
+            return response.status(400).json({ error: 'Problemas ao Listar!', msg:e.code });
+        }
     },
     
     async create(request, response){
-        const { name, login, password } = request.body;
+        try {
+            const { nome, email, login, senha, nivel_id, foto, per_ler, per_salvar, per_atualizar, per_deletar } = request.body;
 
-        var pass = await encript(password);
-        
-        const [id] = await connection('usuarios').insert({
-            name, 
-            login,
-            password: pass
-        });
+            var pass = await security.encript(senha);
+            
+            const [id] = await connection('usuarios').insert({
+                nome, email, login, 
+                senha: pass, 
+                nivel_id, foto, 
+                per_ler, per_salvar, per_atualizar, per_deletar
+            });
+    
+            return response.json({ id });
+        } catch (e) {
+            return response.status(400).json({ error: 'Problemas ao Inserir!', msg:e.code });
+        }
+    },
 
-        return response.json({ id });
+    async update(request, response){
+        try{
+            const { nome, email, login, senha, nivel_id, foto, per_ler, per_salvar, per_atualizar, per_deletar } = request.body;
+            const { id } = request.params;
+            await connection('usuarios')
+                .where('id', id)
+                .update({nome, email, login, senha, nivel_id, foto, per_ler, per_salvar, per_atualizar, per_deletar});
+    
+            return response.status(204).send();
+        } catch (e){
+            return response.status(400).json({ error: 'Problemas ao Atualizar!', msg:e.code });
+        }
     },
 
     async delete(request, response){
-        const { id } = request.params;
-        const token = request.headers.authorization;
-        
-        const session = await connection('sessions')
-            .where('token',token)
-            .select('token')
-            .first();
-
-        if (session.token !== token){
-            return response.status(401).json({ error: 'Operação negada!'});
+        try{
+            const { id } = request.params;
+            await connection('usuarios').where('id', id).delete();
+            return response.status(204).send();
+        }catch (e){
+            return response.status(400).json({ error: 'Problemas ao Deletar!', msg:e.code });
         }
-
-        await connection('usuarios').where('id', id).delete();
-
-        return response.status(204).send();
     }
 }

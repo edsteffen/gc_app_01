581800a8115c4b6b9495d5bf87ab38ef58561d96
edsteffen <edineidaniel@gmail.com>
2020-04-19 18:54:53 -0300

    Validação de Login com BD

--- backend/src/controllers/LoginController.js
+++ backend/src/controllers/LoginController.js
@@ -4,6 +4,7 @@ const security = require('./SecurityController');
 module.exports = {
     async test(request, response){
         const { login, psw } = request.body;
+
         console.log(psw);
         console.log(security.encript(psw));
         var chave = '';
@@ -13,6 +14,29 @@ module.exports = {
         } else {
             chave = await security.decript(psw);
             return response.json({ chave });
+        }
+    },
+    async login(request, response){
+        try{
+            const { login, psw } = request.body;
+            const psw_u = await security.encript(psw);
+            const {id,senha,per_ler,per_salvar,per_atualizar,per_deletar} = await connection('usuarios')
+                .where('login',login)
+                .select('id','senha','per_ler','per_salvar','per_atualizar','per_deletar')
+                .first();
+            if (Number.isInteger(id)) {
+                if (senha===psw_u) {
+                    let skey = Date.now() + '-' + id + '-' + per_ler + '-' + per_salvar + '-' + per_atualizar + '-' + per_deletar;
+                    let token = await security.encript(skey);
+                    response.header('Authorization', token);
+                    return response.status(204).send();
+                } 
+                else { return response.status(401).json({ error: 'Usuário ou Senha inválidos!'}); }
+            } else {
+                return response.status(401).json({ error: 'Login não Autorizado!'});
+            }
+        } catch (e) {
+            return response.status(400).json({ error: 'Problemas no Login...!', msg:e });
         }
     }
 }

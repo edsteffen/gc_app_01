21d89076f626342273ceac8a2ba849903b0cfb51
edsteffen <edineidaniel@gmail.com>
2020-04-18 22:05:17 -0300

    Implementando Middleware CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 require('dotenv').config();
 
-const app = express();
-let port = process.env.DEV_PORT_LISTEN;
+var allowedOrigins = ['http://someorigin.com','http://anotherorigin.com','http://localhost:3333/'];
+var corsOptions = {
+    origin: function(origin, callback){
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+          var msg = 'Requisição Negada!';
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+      exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+      credentials: true,
+}
 
+const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
+let port = process.env.DEV_PORT_LISTEN;
 app.listen(port);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
+
 const MenuController = require('./controllers/MenuController');
 const LoginController = require('./controllers/LoginController');
+const NiveilAcessoController = require('./controllers/NivelAcessoController');
 const routes = express.Router();
+
+
 
 routes.get('/menus', MenuController.index);
 routes.post('/menus', MenuController.create);
 routes.put('/menus/:id', MenuController.update);
 routes.delete('/menus/:id', MenuController.delete);
+
+routes.get('/niveis', NiveilAcessoController.index);
+routes.post('/niveis', NiveilAcessoController.create);
+routes.put('/niveis/:id', NiveilAcessoController.update);
+routes.delete('/niveis/:id', NiveilAcessoController.delete);
 
 routes.post('/login', LoginController.test);
 

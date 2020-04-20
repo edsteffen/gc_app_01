dc5bf8057e561c1d0f15e3e924c7986b45ad36ee
edsteffen <edineidaniel@gmail.com>
2020-04-19 21:28:21 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const MenuController = require('./controllers/MenuController');
 const LoginController = require('./controllers/LoginController');
 const NiveilAcessoController = require('./controllers/NivelAcessoController');
 const UsuarioController = require('./controllers/UsuarioController');
+const MenuNivelController = require('./controllers/MenuNivelController');
 const AuthMiddleware = require('./middleware/AuthMiddleware')
 const routes = express.Router();
 
@@ -16,6 +17,10 @@ routes.get('/niveis', NiveilAcessoController.index);
 routes.post('/niveis', NiveilAcessoController.create);
 routes.put('/niveis/:id', NiveilAcessoController.update);
 routes.delete('/niveis/:id', NiveilAcessoController.delete); 
+
+routes.get('/menusniveis', MenuNivelController.index);
+routes.post('/menusniveis', MenuNivelController.create);
+routes.delete('/menusniveis', MenuNivelController.delete); 
 
 routes.get('/usuarios', AuthMiddleware.auth_index, UsuarioController.index);
 routes.post('/usuarios', AuthMiddleware.auth_create, UsuarioController.create);
